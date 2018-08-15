3f20d70b545327ce80a169e716dd10d1d34538be
Pauloroks <pauloroks@gmail.com>
2018-08-15 19:39:36 -0300

    Update Saving-goals.py

--- Saving-goals.py
+++ Saving-goals.py
@@ -7,19 +7,7 @@ flour = True
 # 2. omelette: egg, milk, butter
 # 3. custard: egg, milk
 # 4. poached eggs: egg
-#
-#The list above is also a priority list. If you have the
-#ingredients for pancakes, you'll make pancakes instead of
-#any of the other dishes. If you're missing flour but have
-#the other ingredients, you'll make an omlette. You'll only
-#make poached eggs if the only ingredient you have is eggs.
-#
-#Complete the program below such that it prints which dish
-#you'll make based on the ingredients you have handy. All
-#the dishes should appear exactly as shown above: all lower
-#case, spelled the same way. If you do not have the
-#ingredients to make any of these dishes, then print the
-#text "Go to the store!"
+
 
 if egg, milk, butter, flour:
     print("pancakes")
