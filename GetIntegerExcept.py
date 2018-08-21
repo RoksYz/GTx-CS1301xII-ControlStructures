6d9fc9cbd25b615d387e32269a9e2a236a364025
Pauloroks <pauloroks@gmail.com>
2018-08-20 21:03:29 -0300

    Add files via upload

---
+++ GetIntegerExcept.py
@@ -0,0 +1,13 @@
+def get_integer(my_var):
+
+    try:
+        my_var = int(my_var)
+        return my_var
+
+    except Exception as error:
+        return (error)
+
+print(get_integer("5"))
+print(get_integer("Boggle."))
+print(get_integer(5.1))
+
